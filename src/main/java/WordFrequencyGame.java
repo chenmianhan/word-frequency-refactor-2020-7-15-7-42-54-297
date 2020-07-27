5f06c0daf7f0eb59762ee415db1b83e54da445b7
CHENJA28 <CHENJA28@OOCL.COM>
2020-07-27 15:37:45 +0800

    [wordFrequency][James] refactor: rename wordInfoList to wordInfos

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -17,26 +17,26 @@ public class WordFrequencyGame {
                 //split the input string with 1 to n pieces of spaces
                 String[] words = sentence.split(SPACE_PATTERN);
 
-                List<WordInfo> wordInfoList = new ArrayList<>();
+                List<WordInfo> wordInfos = new ArrayList<>();
                 for (String word : words) {
                     WordInfo wordInfo = new WordInfo(word, 1);
-                    wordInfoList.add(wordInfo);
+                    wordInfos.add(wordInfo);
                 }
 
                 //get the map for the next step of sizing the same word
-                Map<String, List<WordInfo>> map = getListMap(wordInfoList);
+                Map<String, List<WordInfo>> map = getListMap(wordInfos);
 
                 List<WordInfo> list = new ArrayList<>();
                 for (Map.Entry<String, List<WordInfo>> entry : map.entrySet()) {
                     WordInfo wordInfo = new WordInfo(entry.getKey(), entry.getValue().size());
                     list.add(wordInfo);
                 }
-                wordInfoList = list;
+                wordInfos = list;
 
-                wordInfoList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+                wordInfos.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
 
                 StringJoiner joiner = new StringJoiner("\n");
-                for (WordInfo w : wordInfoList) {
+                for (WordInfo w : wordInfos) {
                     String s = w.getValue() + " " + w.getWordCount();
                     joiner.add(s);
                 }
